1b8582e5c23f700b14cf2c1e16104b55eb08a384
Rahul <rahul@Rameshs-MacBook-Air.local>
2022-02-12 22:46:51 +0530

    Refactoring: improve styling and variable names

--- src/TableFromJson.js
+++ src/TableFromJson.js
@@ -1,5 +1,26 @@
 import React , {useEffect, useState} from 'react'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { Paper, TableContainer,TableRow, TableHead,TableCell ,TableBody,Table } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    columnHeader: {
+      borderBottom: "1px solid rgba(0,0,1,0.2)",
+      boxShadow: "0px 0px 2px 2px black",
+      backgroundColor: "rgba(0,0,1,0.2)",
+    },
+    rowHeaderCell: {
+      fontWeight: 600,
+      backgroundColor: "rgba(0,0,1,0.2)",
+      borderBottom: "1px solid rgba(0,0,1,0.2)", 
+      borderRight: "2px solid rgba(0,0,1,0.2)",
+    },
+    columnHeaderCell: {
+      fontWeight: 600,
+      borderRight: "1px solid rgba(0,0,1,0.3)"
+    },
+  }),
+);
 
 export default function TableFromJson({jsonData}) {
 
@@ -7,43 +28,35 @@ export default function TableFromJson({jsonData}) {
     const [cols,setCols] = useState([]);
     const [total,setTotal] = useState();
     const [data,setData] = useState();
+    const classes = useStyles();
 
   useEffect(() => {
-    //   console.log("Inside UseEffect: "+jsonData);
-      JSON.stringify(jsonData);
-    //   console.log("jsonData: "+jsonData);
-      const usersData = JSON.parse(jsonData);
-    //   console.log("usersData: "+usersData);
-      // * Calculate rows
-      const rowsArr = usersData.map(user => user.job);
-      setRows(rowsArr);
-    //   console.log("rowsArr: "+rowsArr);
+    JSON.stringify(jsonData);
+    const usersData = JSON.parse(jsonData);
+    // * Calculate rows
+    const rowsArr = usersData.map(user => user.job);
+    setRows(rowsArr);
 
-      // * Calculate columns
-      const colsArr = usersData.map(user => user.name);
+    // * Calculate columns
+    const colsArr = usersData.map(user => user.name);
+    const ids = usersData.map(user => user.id);
+    setCols(["Jobs/Name",...rowsArr,"Total"]);
 
-      const ids = usersData.map(user => user.id);
-      setCols(["Jobs/Name",...rowsArr,"Total"]);
-    //   console.log("colsArr: "+colsArr);
+    // * Calculate table data for each cell and total column.
+    var mappedData={};
+    var rowsTotal={};
+    for(var i=0;i<rowsArr.length;i++){
+        mappedData[rowsArr[i]] = {};
+        for(var j=0;j<ids.length;j++){
+            mappedData[rowsArr[i]][ids[j]] = 0;
+            rowsTotal[rowsArr[i]] = 0;
+        }
+    }
 
-      // * Calculate table data for each cell and total column.
-      var mappedData={};
-      var rowsTotal={};
-      for(var i=0;i<rowsArr.length;i++){
-          mappedData[rowsArr[i]] = {};
-          console.log("rowsArr[i].job: "+rowsArr[i]);
-          for(var j=0;j<ids.length;j++){
-              mappedData[rowsArr[i]][ids[j]] = 0;
-              rowsTotal[rowsArr[i]] = 0;
-          }
-      }
-    //   console.log("mappedData: "+JSON.stringify(mappedData));
-
-      for(var i=0;i<usersData.length;i++){
-          mappedData[usersData[i].job][usersData[i].id] = usersData[i].target;
-          rowsTotal[usersData[i].job] += usersData[i].target;
-      }
-    // console.log("mappedData: "+mappedData);
+    for(var i=0;i<usersData.length;i++){
+        mappedData[usersData[i].job][usersData[i].id] = usersData[i].target;
+        rowsTotal[usersData[i].job] += usersData[i].target;
+    }
     setTotal(rowsTotal);
     setData(mappedData);
   },[]);
@@ -52,18 +65,11 @@ export default function TableFromJson({jsonData}) {
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead style={{
-        backgroundColor: "rgba(0,0,1,0.2)",
-        borderBottom: "1px solid rgba(0,0,1,0.2)",
-        boxShadow: "0px -1px 2px 2px black"
-        }}
+      <Table sx={{ width: 650 }} aria-label="simple table">
+        <TableHead className={classes.columnHeader}
         >
-          <TableRow style={{
-        backgroundColor: "rgba(0,0,1,0.2)",
-        borderRightColor: "2px solid rgba(0,0,1,0.2)",
-        }}>
-            {cols.map(col => <TableCell style={{fontWeight: 600}}>{col}</TableCell>)}
+          <TableRow >
+            {cols.map(col => <TableCell className={classes.columnHeaderCell}>{col}</TableCell>)}
           </TableRow>
         </TableHead>
         <TableBody>
@@ -71,23 +77,23 @@ export default function TableFromJson({jsonData}) {
           rows.map((row,index) => (
             <TableRow
               key={row}
-              sx={{
-                   '&:last-child td, &:last-child th': { border: 0 }, 
-                }}
-
+              sx={{'&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row" 
-              style={{backgroundColor: "rgba(0,0,1,0.2)",
-              borderBottom: "1px solid rgba(0,0,1,0.2)", 
-              borderRight: "2px solid rgba(0,0,1,0.2)",
-            }}
+             <TableCell component="th" scope="row" 
+              className={classes.rowHeaderCell}
               >
                 {row}
               </TableCell>
-            {Object.values(data[row]).map(val => <TableCell style={{border: "1px solid rgba(0,0,1,0.2)"}}>{val}</TableCell>)}
-            <TableCell
-            style={{border: "1px solid rgba(0,0,1,0.2)"}}
-            >{total[row]}</TableCell>
+            {
+              Object.values(data[row]).map(val => 
+                <TableCell align="right" style={{border: "1px solid rgba(0,0,1,0.2)"}}>
+                  {val}
+                </TableCell>
+              )
+            }
+            <TableCell align="right" style={{border: "1px solid rgba(0,0,1,0.2)"}}>
+              {total[row]}
+            </TableCell>
             </TableRow>
           ))
           }
